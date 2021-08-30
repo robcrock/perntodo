d2bd507a857ca040eba02dc1179dea04a6fa1b8e
Robert Crocker <robert@vizsimply.com>
2021-08-30 10:31:03 -0400

    Connected our server to postgres.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const pool = require("/.db")
 
 // middleware
 app.use(cors())
 // this line is giving us access to req.body()
 app.use(express.json())
+
+// ROUTES //
+
+// Create a todo
+
+// Get all todo
+
+// Get a todo
+
+// Update a todo
+
+// Delete a todo
 
 app.listen(5000, () => {
   console.log("Server has started on port 5000")
