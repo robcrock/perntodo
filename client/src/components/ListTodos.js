12a93ff9499eb28c454f4dfd2b3732d453e8ac9c
Robert Crocker <robert@vizsimply.com>
2021-09-01 10:12:37 -0400

    Able to delete a todo from the database.

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -13,6 +13,33 @@ const ListTodos = () => {
     }
   }
 
+  const deleteTodo = async (todo_id) => {
+    try {
+      await fetch(`http://localhost:5000/todos/${todo_id}`, {
+        method: "DELETE",
+      })
+      setTodos(todos.filter((todo) => todo.todo_id !== todo_id))
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
+  // const onSubmitForm = async (e) => {
+  //   e.preventDefault()
+  //   try {
+  //     const body = { description }
+  //     const response = await fetch("http://localhost:5000/todos", {
+  //       method: "POST",
+  //       headers: { "Content-Type": "application/json" },
+  //       body: JSON.stringify(body),
+  //     })
+
+  //     window.location = "/"
+  //   } catch (error) {
+  //     console.error(error.message)
+  //   }
+  // }
+
   useEffect(() => {
     getTodos()
   }, [])
@@ -29,10 +56,17 @@ const ListTodos = () => {
         </thead>
         <tbody>
           {todos.map((todo) => (
-            <tr>
+            <tr key={todo.todo_id}>
               <td>{todo.description}</td>
               <td>Edit</td>
-              <td>Delete</td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteTodo(todo.todo_id)}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
